199814c81d31e36ddcf09fccf1bbab1ce5a6cd20
giovannivicentin <giovannifvicentin@gmail.com>
2024-05-22 15:57:06 -0300

    feat: Add style for Details page and import it in Details component

--- src/main.tsx
+++ src/main.tsx
@@ -1,7 +1,7 @@
 import React from 'react';
 import ReactDOM from 'react-dom/client';
 import App from './App.tsx';
-import { Details } from './pages/Details.tsx';
+import { Details } from './pages/Details';
 
 ReactDOM.createRoot(document.getElementById('root')!).render(
   <React.StrictMode>
