1ca06680804df152009a9abdc5d74b5610a4888c
giovannivicentin <giovannifvicentin@gmail.com>
2024-07-08 16:32:57 -0300

    feat: finish the project of rocketseat

--- src/main.jsx
+++ src/main.jsx
@@ -1,15 +1,17 @@
-import React from 'react';
-import ReactDOM from 'react-dom/client';
-import { Details } from './pages/Details';
+import React from 'react'
+import ReactDOM from 'react-dom/client'
 import { ThemeProvider } from 'styled-components'
 import GlobalStyles from './styles/global'
+
 import theme from './styles/theme'
 
-ReactDOM.createRoot(document.getElementById('root')!).render(
+import { Routes } from './routes'
+
+ReactDOM.createRoot(document.getElementById('root')).render(
   <React.StrictMode>
     <ThemeProvider theme={theme}>
       <GlobalStyles />
-    <Details />
+      <Routes />
     </ThemeProvider>
-  </React.StrictMode>
-);
+  </React.StrictMode>,
+)
